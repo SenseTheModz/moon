09b0a3a02805ee0ab4987c8cba1bb81d4236aa4d
Spirtiks <Spirtiks@tuta.io>
2019-01-22 18:39:35 +0000

    Add room clear

--- src/network/protocol/handlers/aqw/remote/MoveToArea.js
+++ src/network/protocol/handlers/aqw/remote/MoveToArea.js
@@ -2,6 +2,8 @@ const Handler = require('../../');
 
 class MoveToArea extends Handler {
   handle(packet) {
+    this.client.player.room.clear();
+
     for (const entity of packet.object.b.o.uoBranch) {
       if (entity.uoName.toLowerCase() === this.client.player.username.toLowerCase()) {
         this.client.player.id = entity.entID;
